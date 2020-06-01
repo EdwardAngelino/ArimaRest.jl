a614baa4eac056e2a9ecb3d2037ac0c3578ab6d6
Edward Angelino <eangeli@hotmail.com>
2020-06-01 16:18:09 -0500

    graficadora en funcion bug julia

--- src/grafico.jl
+++ src/grafico.jl
@@ -1,8 +1,9 @@
 using  Plots
 using DataFrames
 #import DataFrames.DataFrame
-function grafica(df::DataFrames.DataFrame )
+function grafica(df::DataFrames.DataFrame , f::Integer)
 
+if f==1
     plot(1:20,exp.(df.Irrest),grid=false,
         ribbon=(exp.(df.Irrest)-exp.(df."I.Inf"),exp.(df."I.Sup")-exp.(df.Irrest)),
         fillalpha=.2,color="orange",label = "Sin Restricciones")
@@ -10,4 +11,14 @@ function grafica(df::DataFrames.DataFrame )
     plot!(1:20,exp.(df.Restr),grid=false,ribbon=(exp.(df.Restr)-exp.(df."R.Inf"),
         exp.(df."R.Sup")-exp.(df.Restr)),fillalpha=.3,color="blue",
         label = "Con Restricciones",title = "Prónosticos exp(val)")
+else
+    plot(1:20,(df.Irrest),grid=false,
+        ribbon=((df.Irrest)-(df."I.Inf"),(df."I.Sup")-(df.Irrest)),
+        fillalpha=.2,color="orange",label = "Sin Restricciones")
+
+    plot!(1:20,(df.Restr),grid=false,ribbon=((df.Restr)-(df."R.Inf"),
+        (df."R.Sup")-(df.Restr)),fillalpha=.3,color="black",
+        label = "Con Restricciones",title = "Prónosticos (val)")
+
+end
 end
